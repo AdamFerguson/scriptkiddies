a2b54a52f307063e9a210a1377858b61c80b659a
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-05 02:53:51 -0400

    Full map

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,5 +6,6 @@ $(function() {
   var map = new L.Map("map", {
         center: [35.227087, -80.843127],
         zoom: 10
-      }).addLayer(new L.TileLayer("http://c.tile.cloudmade.com/b90910898d15474ca52846a11bd303e8/67367/256/10/282/405.png"));
+      }).addLayer(new L.TileLayer("http://c.tile.cloudmade.com/b90910898d15474ca52846a11bd303e8/67367/256/{z}/{x}/{y}.png"));
+      // }).addLayer(new L.TileLayer("http://c.tile.cloudmade.com/b90910898d15474ca52846a11bd303e8/67367/256/10/282/405.png"));
 });
