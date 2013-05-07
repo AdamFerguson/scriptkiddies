8f7cb9428749dbfa58780f8744dd24324b5e1664
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-07 14:36:14 -0400

    Add query for discovering customers with multiple segment id's

--- aggregation.js
+++ aggregation.js
@@ -2,13 +2,21 @@ var mongojs = require('mongojs');
 
 var db = mongojs('scriptkiddies', ['households', 'stores']);
 
+db.households.find(function(err,houses) {
+  var filteredHouses = houses.filter(function(house) {
+    return (house.segmentIds.length > 1);
+  }).map(function(house) { return house.householdId; });
+  console.log(filteredHouses);
+});
+
 db.households.aggregate(
   { $project: { householdId: 1, transactions: 1}},
   { $unwind: "$transactions"},
   { $group : { _id : '$householdId', salesPerHouse : { $sum : "$transactions.netSales" } } },
   function(err, docs) {
-    if (err) console.log('err');
-    else console.log(docs);
+    if (err) console.log(err);
+    // else console.log(docs);
 
-    process.exit();
+    // process.exit();
   });
+
