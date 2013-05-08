262a0b0c5eb0dcd99322e6b236967b1c1ea20f36
mva3212 <devbox@devbox-Precision-WorkStation-380.(none)>
2013-05-07 21:12:27 -0400

    added foursquare and query string gems, updated map for plotting.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,6 +9,20 @@ $(function() {
         zoom: 10
       }).addLayer(new L.TileLayer("http://c.tile.cloudmade.com/b90910898d15474ca52846a11bd303e8/998/256/{z}/{x}/{y}.png"));
 
+$.getJSON('/stores', function(data) {
+  $.each(data, function(key, val) {
+    var desc = "Harris Teeter #" + val.storeId;
+    var marker = L.marker([val.loc[1],val.loc[0]]).addTo(map).bindPopup(desc);
+  });
+});
+
+// this prints out the houses but pretty much just crashes
+// $.getJSON('/households', function(data) {
+//   $.each(data, function(key, val) {
+//     var marker = L.marker([val.loc[1],val.loc[0]]).addTo(map).bindPopup('houses');
+//   });
+// });
+
       // Themes:
       // - Dark Blue: 67367
       // - Transparent: 998
