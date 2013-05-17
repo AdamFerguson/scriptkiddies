f7814bbc47686d2ff4105cc7b817b534a5b1652d
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-17 14:51:13 -0400

    Full implementation of tractSchema

--- models/tract.js
+++ models/tract.js
@@ -4,7 +4,35 @@ var mongoose = require('mongoose');
 
 var tractSchema = mongoose.Schema({
   tractId: { type: Number, index: true, unique: true },
-  loc: { type: { type: String }, coordinates: mongoose.Schema.Types.Mixed }
+  loc: { type: { type: String }, coordinates: mongoose.Schema.Types.Mixed },
+  totalPopulations: [ 
+    year: { type: Number, index: true },
+    count: { type: Number }
+  ],
+  numHouseholds: [
+    year: { type: Number, index: true },
+    count: { type: Number }
+  ],
+  numFamilies: [
+    year: { type: Number, index: true },
+    count: { type: Number }
+  ],
+  averageHouseholdSizesByAge: [
+    year: { type: Number, index: true },
+    ageRange: {
+      minAge: { type: Number, index: true },
+      maxAge: { type: Number, index: true }
+    },
+    count: { type: Number }
+  ],
+  gendersByAge: [
+    year: { type: Number, index: true },
+    ageRange: {
+      minAge: { type: Number, index: true },
+      maxAge: { type: Number, index: true }
+    },
+    count: { type: Number }
+  ]
 });
 
 tractSchema.index({loc: '2dsphere'});
