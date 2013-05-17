1f6ba38eb5e21dcd6a447911472a80dd308ae48a
mva3212 <devbox@devbox-Precision-WorkStation-380.(none)>
2013-05-17 17:40:15 -0400

    tried to make query in tract.js

--- routes/tract.js
+++ routes/tract.js
@@ -1,6 +1,22 @@
 var Tract     = require('../models').Tract;
 var streamable = require('../app').streamable;
 
+
+
 exports.list = [streamable, function(req, res) {
-  Tract.find({}).stream({ transform: JSON.stringify }).pipe(res);
+  var lngLow  = -81.491089‎;
+  var lngHigh = -80.158997‎;
+  var latLow  = 34.817186;
+  var latHigh =35.756543;
+  var geojsonPoly = {
+  type: 'Polygon',
+  coordinates: [[
+      [lngLow,latLow],
+      [lngHigh,latLow],
+      [lngHigh,latHigh],
+      [lngLow,latHigh],
+      [lngLow,latLow]
+    ]]
+  };
+  Tract.find({loc: { $within: { $geometry: geojsonPoly }).stream({ transform: JSON.stringify }).pipe(res);
 }];
