928c0913a13b5e827954e89d4489ba1e77f04d6a
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-17 21:54:22 -0400

    Working on filtering tracts

--- routes/tract.js
+++ routes/tract.js
@@ -2,8 +2,24 @@ var Tract     = require('../models').Tract;
 var streamable = require('../app').streamable;
 
 exports.list = [streamable, function(req, res) {
-
-Tract.find().stream({transform: JSON.stringify}).pipe(res);
-//Tract.find({loc:{coordinates: { $within: { $geometry: geojsonPoly }}}}).stream({ transform: JSON.stringify }).pipe(res);
+  var lngLow  = -81.491089;
+  var lngHigh = -80.158997;
+  var latLow  = 34.817186;
+  var latHigh = 35.756543;
   //Tract.searchByBounds(latHigh,lngHigh,latLow,lngLow).stream({ transform: JSON.stringify }).pipe(res);
+  //Tract.find().stream({ transform: JSON.stringify }).pipe(res);
+  var geojsonPoly = { type: 'Polygon', coordinates: [[
+    [lngLow, latLow],
+    [lngHigh, latLow],
+    [lngHigh, latHigh],
+    [lngLow, latHigh],
+    [lngLow, latLow]
+  ]]};
+  // returns a query object that you can treat like a promise, or stream
+  return Tract.find({
+  	loc: { 
+  		$geoIntersects: { 
+  			$geometry: geojsonPoly 
+  		}}
+  	}, undefined, { batchSize: 1000}).stream({transform: JSON.stringify}).pipe(res);
 }];
