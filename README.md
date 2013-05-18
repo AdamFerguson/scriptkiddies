3375105c36dc52d0c48914ae8af1f2ed48cf8598
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-18 18:50:45 -0400

    Cleanup

--- README.md
+++ README.md
@@ -2,26 +2,15 @@
 
 - MongoDB >= 2.4
 - NodeJS >= 0.10
-- ExpressJS installed globally (available through `npm install -g express`)
+- Supervisor - `npm install -g supervisor'
+
+To start app: `supervisor app`
 
 node modules are kept in the repo to go along with suggested best practices
 
 
 # Data import
 
-To import data run: `./jake import_data`
+To import data run: `./jake import_stores && ./jake import_households && ./jake import_tracts`
 
 
-# Geospatial querying
-
-When using mongoose, you can execute geospatial queries like:
-
-- Inclusion:
-
-```javascript
-var lngLow  = -80.75;
-var lngHigh = -80.8;
-var latLow  = 35.3;
-var latHigh = 35.4;
-var geojsonPoly = { type: 'Polygon', coordinates: [[lngLow,latLow], [lngLow,latHigh], [lngHigh,latLow], [lngHigh,latHigh]] };
-```
--- routes/tract.js
+++ routes/tract.js
@@ -2,25 +2,5 @@ var Tract     = require('../models').Tract;
 var streamable = require('../app').streamable;
 
 exports.list = [streamable, function(req, res) {
-  var lngLow  = -81.491089;
-  var lngHigh = -80.158997;
-  var latLow  = 34.817186;
-  var latHigh = 35.756543;
-  //Tract.searchByBounds(latHigh,lngHigh,latLow,lngLow).stream({ transform: JSON.stringify }).pipe(res);
   Tract.find({}, {tractId: 1, loc: 1, totalPopulations: 1, area: 1}).stream({ transform: JSON.stringify }).pipe(res);
-  /*var geojsonPoly = { type: 'Polygon', coordinates: [[
-    [lngLow, latLow],
-    [lngHigh, latLow],
-    [lngHigh, latHigh],
-    [lngLow, latHigh],
-    [lngLow, latLow]
-  ]]};
-  // returns a query object that you can treat like a promise, or stream
-  return Tract.find({
-  	loc: { 
-  		$geoIntersects: { 
-  			$geometry: geojsonPoly 
-  		}}
-  	}, undefined, { batchSize: 1000}).stream({transform: JSON.stringify}).pipe(res);
-  */
 }];
