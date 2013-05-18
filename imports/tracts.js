bb326e067ccd16fd626ca5029471d690281b505f
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-17 21:11:54 -0400

    Average household size import done

--- imports/tracts.js
+++ imports/tracts.js
@@ -34,13 +34,13 @@ exports.importTracts = function() {
         });
       }
     });
-  }
+  };
 
   var metadataImport = function(done) {
     metadata = require('../data/census/sf1_labels.json');
     console.log('done importing metadata');
     done();
-  }
+  };
 
   var familiesImport = function(done) {
     var total = 0, soFar = 0;
@@ -62,7 +62,8 @@ exports.importTracts = function() {
             {year: 2000, count: row['P035001.2000']},
             {year: 2010, count: row['P035001']}
           ]
-        }
+        };
+
         Tract.update({tractId: row.GEOID}, data, function(err,tract) {
           soFar++;
           if (err) console.log(err);
@@ -71,7 +72,7 @@ exports.importTracts = function() {
           }
 
           if (soFar === total) {
-            console.log('done importing populating');
+            console.log('done importing population');
             done();
           }
         });
@@ -79,11 +80,65 @@ exports.importTracts = function() {
   };
 
   var averageHouseholdSizesByAgeImport = function(done) {
+    var total = 0, soFar = 0;
+
+    csv().
+      from.path(__dirname+'/../data/census/all_140_in_37.P17.csv', {columns: true}).
+      on('record', function(row, index) {
+        total++;
+        var data = {
+          averageHouseholdSizesByAge: [
+            { year: 2000,
+              ageRange: {
+                minAge: 0,
+                maxAge: 17
+              },
+              count: row['P017002.2000']
+            },
+            { year: 2000,
+              ageRange: { minAge: 18 },
+              count: row['P017003.2000']
+            },
+            { year: 2000,
+              ageRange: { minAge: 0 },
+              count: row['P017001.2000']
+            },
+            { year: 2010,
+              ageRange: {
+                minAge: 0,
+                maxAge: 17
+              },
+              count: row['P017002']
+            },
+            { year: 2010,
+              ageRange: { minAge: 18 },
+              count: row['P017003']
+            },
+            { year: 2010,
+              ageRange: { minAge: 0 },
+              count: row['P017001']
+            }
+          ]
+        };
+
+        Tract.update({tractId: row.GEOID}, data, function(err,tract) {
+          soFar++;
+          if (err) console.log(err);
+          else {
+            console.log('Average Household Size import: ' + soFar);
+          }
+
+          if (soFar === total) {
+            console.log('done importing Average Household Size');
+            done();
+          }
+        });
+      });
   };
 
   var gendersByAgeImport = function(done) {
   };
 
-  async.series([tractsJsonImport, metadataImport, familiesImport]);
+  async.series([tractsJsonImport, metadataImport, familiesImport, averageHouseholdSizesByAgeImport]);
 
 };
