2c1d350ac390aaca08db55312a95e06d7e23ba0f
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2013-05-19 04:06:53 -0400

    Detail table, looking halfway decent

--- public/javascripts/tractDetailView.js
+++ public/javascripts/tractDetailView.js
@@ -38,15 +38,40 @@ function(app) {
 
     var tractDetailsTmpl = _.template($('#tract-details-tmpl').html());
     var oldSelectedTractIds = [];
+    var firstRender = true;
     app.updateTractDetails = function() {
       if (_.difference(app.selectedTractIds, oldSelectedTractIds).length === 0
-          && _.difference(oldSelectedTractIds, app.selectedTractIds).length === 0) {
+          && _.difference(oldSelectedTractIds, app.selectedTractIds).length === 0 && !firstRender) {
         return;
       }
       oldSelectedTractIds = _.clone(app.selectedTractIds);
+      firstRender = false;
 
       var tracts = app.selectedTractIds.map(function(tractId) { return app.cachedTractData[tractId]; });
-      $('#tract-details').html(tractDetailsTmpl({tracts: tracts}));
+      var data = {
+        tracts: tracts,
+        averageHouseholdSize: function(tract, year) {
+          var result = _.find(tract.averageHouseholdSizesByAge, function(size) {
+            return (size.year === year && size.ageRange.minAge === 0 && !size.ageRange.maxAge);
+          });
+
+          return result ? result.count : 'N/A';
+        },
+        totalSales: function(tract) {
+          var total = {};
+          if (tract.households) {
+            _.keys(tract.households).forEach(function(householdId) {
+              var household = tract.households[householdId];
+              household.transactions.forEach(function(trans) {
+                var description = trans.description;
+                total[description] = total[description] ? (total[description] + total.netSales) : trans.netSales;
+              });
+            });
+          }
+          return total;
+        }
+      };
+      $('#tract-details').html(tractDetailsTmpl(data));
     };
      
   });
